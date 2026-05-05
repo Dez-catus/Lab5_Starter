2995701fa37d6cbf4a64ecdae175e54eadd1bafa
josh <victoriajosh01@gmail.com>
2026-05-04 23:14:48 -0700

    added responses to questions and finished explore.js

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 Lab partners: Me
+
+1. You would not use a unit test to test a "message" feature of a messaging application. The message feature involves many moving parts which unit tests dont test, the messaging feature is not "seperated" from the rest of the code. Other unit tests would be affected by this feature 
+2. You would use a unit test to test out a "max message length” feature because that is debugging on a small scale and isolable.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,5 +4,54 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  addEventListener("click", (event) => { })
+  const textArea = document.querySelector("#text-to-speak");
+  const voiceSelect = document.querySelector("#voice-select");
+  const speakButton = document.querySelector("button");
+  const face = document.querySelector("img");
+  let voices = [];
+  const synth = window.speechSynthesis;
+
+  function loadVoices() {
+  const voices = speechSynthesis.getVoices();
+
+  if (!voices.length) return; 
+
+  voiceSelect.innerHTML = '<option disabled selected>Select Voice:</option>';
+
+  voices.forEach((voice, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.textContent = `${voice.name} (${voice.lang})`;
+    voiceSelect.appendChild(option);
+  });
+}
+speechSynthesis.addEventListener("voiceschanged", loadVoices);
+
+
+loadVoices();
+
+  speakButton.addEventListener("click", () => {
+    const text = textArea.value;
+
+    if (!text) return;
+
+    speechSynthesis.cancel();
+
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    const selectedIndex = voiceSelect.value;
+    if (selectedIndex !== "select") {
+      utterance.voice = voices[selectedIndex];
+    }
+
+    utterance.onstart = () => {
+      face.src = "assets/images/smiling-open.png";
+    };
+
+    utterance.onend = () => {
+      face.src = "assets/images/smiling.png";
+    };
+
+    speechSynthesis.speak(utterance);
+  });
 }
